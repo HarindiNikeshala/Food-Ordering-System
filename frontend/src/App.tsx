5c85be1b188d75bb9bf4ad79366c3046af7a9874
harindinikeshala <harindinikeshala@gmail.com>
2025-10-29 11:37:05 +0530

    Databse connecting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      Click me
+      <Button>Click me</Button>
     </div>
   )
 }
 
 export default App
+
